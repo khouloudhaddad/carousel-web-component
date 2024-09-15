0024889a586af7df1d4b19a5a20fd3812571664c
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-09-15 16:46:47 +0100

    Enhance Black-box feature in web component

--- components/KhCustomCarousel.js
+++ components/KhCustomCarousel.js
@@ -1,7 +1,7 @@
 class KhCustomCarousel extends HTMLElement{
     constructor(){
         super();
-        this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'closed' });
     }
 
     connectedCallback(){
@@ -27,7 +27,7 @@ class KhCustomCarousel extends HTMLElement{
                             box-sizing: border-box;
                         }
 
-                        img {
+                        ::slotted(img) {
                             width: 100%;
                             display: block;
                         }
